466a278d813026d37cb5943262fe7f375e61a4ed
artichaudhary0 <chaudhary.aarti1998@gmail.com>
2022-03-31 23:00:50 +0530

    AppBar2

---
+++ lib/appBar1.dart
@@ -0,0 +1,92 @@
+import 'package:flutter/material.dart';
+
+class Appbar1 extends StatelessWidget {
+  const Appbar1({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 4,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text("My AppBar"),
+          leading: IconButton(
+            icon: Icon(Icons.menu),
+            onPressed: () {},
+          ),
+          titleSpacing: 50,
+          actions: [
+            IconButton(
+              onPressed: () {},
+              icon: Icon(Icons.notifications_none),
+            ),
+            IconButton(
+              onPressed: () {},
+              icon: Icon(Icons.search),
+            ),
+          ],
+          flexibleSpace: Container(
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                colors: [Colors.deepPurple, Colors.pink],
+                begin: Alignment.bottomRight,
+                end: Alignment.topLeft,
+              ),
+            ),
+          ),
+          elevation: 30,
+          bottom: TabBar(
+            indicatorColor: Colors.white,
+            indicatorWeight: 5,
+            tabs: [
+              Tab(
+                icon: Icon(Icons.home),
+                text: "Home",
+              ),
+              Tab(
+                icon: Icon(Icons.star),
+                text: "Feed",
+              ),
+              Tab(
+                icon: Icon(Icons.face),
+                text: "Profile",
+              ),
+              Tab(
+                icon: Icon(Icons.settings),
+                text: "Settings",
+              )
+            ],
+          ),
+        ),
+        body: TabBarView(
+          children: [
+            buildPage("Home Page"),
+            buildPage("Feed Page"),
+            buildPage("Profile Page"),
+            buildPage("Settings Page"),
+          ],
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          backgroundColor: Colors.purple,
+          items: [
+            BottomNavigationBarItem(
+              label: "Account",
+              icon: Icon(Icons.account_circle_outlined,
+                  color: Colors.white, size: 30),
+            ),
+            BottomNavigationBarItem(
+                label: "Set Time",
+                icon: Icon(Icons.access_time, color: Colors.white, size: 30)),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget buildPage(String text) => Center(
+        child: Text(
+          text,
+          style: TextStyle(fontSize: 30.0),
+        ),
+      );
+}
