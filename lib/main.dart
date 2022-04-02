e3ea0eadee6ae5fcaf163fbf338acc3307d03fe8
artichaudhary0 <chaudhary.aarti1998@gmail.com>
2022-04-02 22:33:46 +0530

    AppBar3

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'appbar2.dart';
+import 'appBar3.dart';
+import 'dart:html';
 
 void main() {
   runApp(const MyApp());
@@ -17,7 +18,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.purple,
       ),
-      home: AppBar2(),
+      home: AppBar3(),
     );
   }
 }
